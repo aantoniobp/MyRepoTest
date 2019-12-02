d62a99dc0d5c6a21e63379a4a6b5bd13fbf14bfa
aantoniobp <aantonio.beltran@gmail.com>
2019-12-02 19:02:47 +0000

    Multiple scrip lines captured in vector

--- main.cpp
+++ main.cpp
@@ -5,28 +5,48 @@ using namespace std;
 
 int main() {
   //Definitions
+
   int N, Q;
-  std::string Msg0 = "How many Lines? ...\n";
-  std::string Msg1 = "How many Queries? ...\n";
+  int i = 0;
+  // User input 
+  std::string Msg0 = "How many Lines? ...";
+  std::string Msg1 = "How many Queries? ...";
   std::vector<string> MsgsVector;
   MsgsVector.push_back(Msg0);
   MsgsVector.push_back(Msg1);
+  
+  //Code Vector
+  std::vector<string> CodeVector;
+  std::string CodeLine;
 
-  // 
-  std::cout << "Program Satarts...!\n";
-  std::cout << MsgsVector.at(0).c_str();
+  // Body 
+  std::cout << "Program Satarts...!" << std::endl;
+  std::cout << MsgsVector.at(0).c_str() << std::endl;
   std::cin >> N;
-  std::cout << MsgsVector.at(1).c_str();
+  std::cout << MsgsVector.at(1).c_str() << std::endl;
   std::cin >> Q; 
 
+  std::cout << "Capture your script lines..." << std::endl;
+
+  while (i < N) {
+    std::getline(std::cin, CodeLine);
+    if (!CodeLine.empty()){
+      CodeVector.push_back(CodeLine);
+      i++;
+    }
+    //std::cout << i << "\n";
+  }
   
 
 
-  
-  
 
+  
   //Outputs
-  std::cout << N << Q;
-
-
+  std::cout << "Inputs: \n N =" << N << "\n Q =" << Q << std::endl;
+  i = 0;
+  while (i < N) {
+    std::cout << CodeVector.at(i) << std::endl;
+    i++;
+  }
+  
   }
