f7fb5a4302550992453900bec9070613875c97b3
aantoniobp <aantonio.beltran@gmail.com>
2019-12-02 16:37:08 +0000

    Add messages vector

--- main.cpp
+++ main.cpp
@@ -4,8 +4,29 @@
 using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-  std::vector<string> MyVecStr;
+  //Definitions
+  int N, Q;
+  std::string Msg0 = "How many Lines? ...\n";
+  std::string Msg1 = "How many Queries? ...\n";
+  std::vector<string> MsgsVector;
+  MsgsVector.push_back(Msg0);
+  MsgsVector.push_back(Msg1);
+
+  // 
+  std::cout << "Program Satarts...!\n";
+  std::cout << MsgsVector.at(0).c_str();
+  std::cin >> N;
+  std::cout << MsgsVector.at(1).c_str();
+  std::cin >> Q; 
+
+  
+
+
+  
+  
+
+  //Outputs
+  std::cout << N << Q;
 
 
   }
